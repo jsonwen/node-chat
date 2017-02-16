cd1f0976274872353d8bc210205c9827880fa63b
Jason Wen <jason@leadsocial.com>
2017-02-16 09:27:03 +0100

    Added express in package.jso and prep socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
 
-const app = express();
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Server is up on port', port);
 });
