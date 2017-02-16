0e0d0ae08378233a6839a46a035db0b26ad9dc44
Jason Wen <jason@leadsocial.com>
2017-02-16 10:11:09 +0100

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Jason',
-    text: 'Hey how are you?',
-    createdAt: '123'
-  });
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
 
-  socket.on('createMessage', (newEmail) => {
-    console.log('createMessage', newEmail);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   io.on('disconnect', () => {
