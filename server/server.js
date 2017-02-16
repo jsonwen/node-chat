d805567c3f0c2e0b656350e79f9aa781f0e897ff
Jason Wen <jason@leadsocial.com>
2017-02-16 09:43:50 +0100

    Connect and disconnect socketio handlers

--- server/server.js
+++ server/server.js
@@ -7,8 +7,17 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
+var io = socketio(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
 server.listen(port, () => {
   console.log('Server is up on port', port);
