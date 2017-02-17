4a5a2c4fc87a7fb9f58db841cdfab94529de31f7
Jason Wen <jason@leadsocial.com>
2017-02-17 09:33:36 +0100

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -22,6 +22,19 @@ socket.on('newMessage', function(message) {
   $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+  var li = $('<li></li>');
+  // do not use like this, to prevent injecting html
+  //var a = $(`<a href="${message.url}" target="_blank">My current location</a>`);
+  var a = $('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+
+  $('#messages').append(li);
+});
+
 // socket.emit('createMessage', {
 //   from: 'Frank',
 //   text: 'Hello'
@@ -37,5 +50,22 @@ $('#message-form').on('submit', function(e) {
     text: $('[name=message]').val()
   }, function(response) {
     console.log('Got it', response);
+  });
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function(e) {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not support by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    alert('Unable to fetch location');
   });
 });
