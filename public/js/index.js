35e5f345b469f5156169bde70aa079c27c32869b
Jason Wen <jason@leadsocial.com>
2017-02-17 09:50:33 +0100

    Add css

--- public/js/index.js
+++ public/js/index.js
@@ -45,11 +45,13 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var $messageTextBox = $('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: $messageTextBox.val()
   }, function(response) {
-    console.log('Got it', response);
+    $messageTextBox.val('');
   });
 });
 
@@ -59,13 +61,16 @@ locationButton.on('click', function(e) {
     return alert('Geolocation not support by your browser');
   }
 
-  navigator.geolocation.getCurrentPosition(function(position) {
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
+  navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 });
